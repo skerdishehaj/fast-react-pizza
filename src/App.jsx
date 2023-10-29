6b3db4115b8fa1d326a88b571595dd68af31844e
Skerdi Shehaj <skerdianshehaj@gmail.com>
2023-10-29 22:16:42 +0100

    Handled error using the errorElement property and useRouteError hook to access the occured error

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Home from './ui/Home';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import Order from './features/order/Order';
 import CreateOrder from './features/order/CreateOrder';
+import Home from './ui/Home';
 import AppLayout from './ui/AppLayout';
+import Error from './ui/Error';
 
 // * Imperative way of using Routing which enables data loading capabilities
 // * Data Loaders, Data Fetchers, Data Actions
@@ -13,31 +14,17 @@ const router = createBrowserRouter([
   {
     element: <AppLayout />,
     children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
+      { path: '/', element: <Home /> },
       {
         path: '/menu',
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
-      {
-        path: '/cart',
-        element: <Cart />,
-      },
-      {
-        path: '/order',
-        element: <Order />,
-      },
-      {
-        path: '/order/new',
-        element: <CreateOrder />,
-      },
-      {
-        path: '/order/:orderId',
-        element: <Order />,
-      },
+      { path: '/cart', element: <Cart /> },
+      { path: '/order', element: <Order /> },
+      { path: '/order/new', element: <CreateOrder /> },
+      { path: '/order/:orderId', element: <Order /> },
     ],
   },
 ]);
